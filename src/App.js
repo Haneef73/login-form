db22592b155ff54ad2117ef55cd70d444070773a
Haneef <haneefa@tmmmt.com>
2021-08-04 00:24:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,210 @@
-import logo from './logo.svg';
+import React from "react";
+import "./index.css";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: `http://localhost:3001/info/`
+})
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newUser: true,
+      right: 0,
+    }
+  }
+
+  handleClick(button) {
+    if(this.state.newUser && button !== 'signUp') {
+      this.setState({newUser: false})
+    } else if(!this.state.newUser && button !== 'signIn') {
+      this.setState({newUser: true})
+    }
+  }
+  
+  
+  render() {
+    return(
+           <div className='formContainer'>
+              <div className='formHeader'>
+                  <div 
+                    className={ this.state.newUser ? 'headerActive' : 'headerInActive' } 
+                    onClick={() => this.handleClick('signUp')}
+                    >
+                    <button className='headerButton'> Sign Up </button>
+                  </div>
+                  <div 
+                    className={ this.state.newUser ? 'headerInActive' : 'headerActive' } 
+                    onClick={() => this.handleClick('signIn')}
+                    >
+                    <button className='headerButton'> Sign In </button>
+                  </div>
+              </div>
+              <div className='formBody'>
+                {
+                  this.state.newUser ? <SignUp />: <SignIn />
+                }
+              </div>
+           </div>
+    ) 
+
+  }
 }
 
-export default App;
+
+class SignUp extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      Name : "",
+      Email : "",
+      Password : "",
+    }
+    
+  }
+  
+  InfoChange = event => {
+      const {name, value} = event.target;
+      this.setState({
+        [name] : value
+      })
+  }
+
+  InfoSubmit = event => {
+    event.preventDefault();
+
+    var data = {
+      Name : this.state.Name,
+      Email : this.state.Email,
+      Password : this.state.Password,
+    }
+    if(data.Password !== this.state.RePassword){
+      alert("Password Mismatching, Pls Enter Correct Password");
+    }else
+    {
+      api.post("/",data).then(res => {
+        console.log(res);
+      }).catch(err => {
+        alert("Already registerd this  email! Pls goto login")
+        console.log(err)
+      });
+    }
+}
+
+render() {
+    return( 
+      <form  onSubmit = {this.InfoSubmit}>   
+      <div className='signUpContainer' >
+      
+        <h4 className='headerText'>Join Us Today</h4>
+        <div className='inputSection'>
+          <input type='text' className='fullName' required 
+          onChange = {this.InfoChange}
+          name = "Name"
+          value = {this.state.Name}
+          />
+          <label className='inputLabel'>Full Name</label>
+        </div>
+        <div className='inputSection'>
+          <input type='text' className='emailAddress' required
+                    onChange = {this.InfoChange}
+                    name = "Email"
+                    value = {this.state.Email}
+                    />
+          <label className='inputLabel'>Email Address</label>
+        </div>
+        <div className='inputSection'>
+          <input type='password' className='password' required
+                    onChange = {this.InfoChange}
+                    name = "Password"
+                    value = {this.state.Password}
+          />
+          <label className='inputLabel'>Password</label>
+        </div>
+        <div className='inputSection'>
+          <input type='password' className='password' required
+                    onChange = {this.InfoChange}
+                    name = "RePassword"
+                    value = {this.state.RePassword}
+          />
+          <label className='inputLabel'>Re Enter Password</label>
+        </div>
+        <div className='formFooter'>
+                <button className='saveForm'> SignUp </button>
+              </div>
+      </div>
+      </form>
+    )
+  }
+}
+
+class SignIn extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      Email : "",
+      Password : "",
+    }
+  }
+
+  UserInfo = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name] : value
+    })
+
+}
+
+LoginSubmit = event => {
+  event.preventDefault();
+  var data = {
+    Email: this.state.Email,
+    Password: this.state.Password
+  }
+
+  api.post("/login", data).then(res => {
+    console.log(res.data,"Login Successfully");
+    alert("Login Successfully");
+  }).catch(err => {
+    console.log(err);
+    alert("Check your Email and Password is valid..!");
+  })
+}
+
+  render() {
+    return(
+      <form onSubmit={this.LoginSubmit}>
+      <div className='signInContainer'>
+        <h4 className='headerText'>Welcome Back</h4>
+        <div className='inputSection'>
+          <input type='text' className='email' required
+                  onChange = {this.UserInfo}
+                  name = "Email"
+                  value = {this.state.Email}
+          />
+          <label className='inputLabel'>Email ID</label>
+        </div>
+        <div className='inputSection'>
+          <input type='password' className='password' required
+                  onChange = {this.UserInfo}
+                  name = "Password"
+                  value = {this.state.Password}
+          />
+          <label className='inputLabel'>Password</label>
+        </div>
+        <div className='formFooter'>
+                <button className='saveForm'> Login </button>
+              </div>
+      </div>
+      </form>
+    )
+  }
+}
+
+
+export default LoginForm;
